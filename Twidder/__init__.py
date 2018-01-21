09f994560463de25cae7d70eebf3a0e9fae0ccd1
Adeline <adeline80916@gmail.com>
2018-01-21 18:59:07 +0800

    Change to WSGI server

--- Twidder/__init__.py
+++ Twidder/__init__.py
@@ -5,12 +5,24 @@
 
 # import Twidder.views
 from flask import Flask
+from gevent.wsgi import WSGIServer
+from geventwebsocket.handler import WebSocketHandler
+from geventwebsocket.websocket import WebSocketError
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     print("__init__.py - index")
     return app.send_static_file('client.html')
 
+print("__init__.py - Outside the scope")
+http_server = WSGIServer(('', 5003), app, handler_class=WebSocketHandler)
+http_server.serve_forever()
+
+
 if __name__ == "__main__":
-    app.run()
+	# pass
+	print("__init__.py - __main__")
+	# app.run()
